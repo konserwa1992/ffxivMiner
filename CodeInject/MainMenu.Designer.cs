f60b8079a96aa739ef195c666ae12458a9faebc6
konserwa1992 <38973532+konserwa1992@users.noreply.github.com>
2023-07-18 23:26:21 +0200

    Blink to deposit and saved spots.

--- CodeInject/MainMenu.Designer.cs
+++ CodeInject/MainMenu.Designer.cs
@@ -32,12 +32,17 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.lNearObjects = new System.Windows.Forms.ListBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.lObjectCount = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lObjectToActive = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lObjectCount = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.tInit = new System.Windows.Forms.Timer(this.components);
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -60,23 +65,77 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.lObjectToActive);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Controls.Add(this.lObjectCount);
             this.groupBox1.Controls.Add(this.lNearObjects);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(543, 264);
+            this.groupBox1.Size = new System.Drawing.Size(656, 358);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Near by objects";
             // 
-            // textBox1
+            // button4
             // 
-            this.textBox1.Location = new System.Drawing.Point(437, 22);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
-            this.textBox1.TabIndex = 6;
+            this.button4.Location = new System.Drawing.Point(218, 242);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Add Spot";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(218, 269);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(291, 79);
+            this.listBox1.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(524, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Add Mining deposit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lObjectToActive
+            // 
+            this.lObjectToActive.FormattingEnabled = true;
+            this.lObjectToActive.ItemHeight = 15;
+            this.lObjectToActive.Location = new System.Drawing.Point(524, 269);
+            this.lObjectToActive.Name = "lObjectToActive";
+            this.lObjectToActive.Size = new System.Drawing.Size(120, 79);
+            this.lObjectToActive.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CodeInject.Properties.Resources.maxresdefault;
+            this.pictureBox1.Location = new System.Drawing.Point(218, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(426, 214);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // lObjectCount
             // 
@@ -87,19 +146,9 @@
             this.lObjectCount.TabIndex = 2;
             this.lObjectCount.Text = "label1";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::CodeInject.Properties.Resources.maxresdefault;
-            this.pictureBox1.Location = new System.Drawing.Point(286, 282);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(426, 243);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 311);
+            this.button1.Location = new System.Drawing.Point(12, 343);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 23);
             this.button1.TabIndex = 4;
@@ -109,7 +158,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 282);
+            this.button2.Location = new System.Drawing.Point(12, 314);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 23);
             this.button2.TabIndex = 5;
@@ -117,14 +166,15 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
+            // timer2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(240, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 15);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "label1";
+            this.timer2.Interval = 200;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // tInit
+            // 
+            this.tInit.Enabled = true;
+            this.tInit.Tick += new System.EventHandler(this.tInit_Tick);
             // 
             // MainMenu
             // 
@@ -133,7 +183,6 @@
             this.ClientSize = new System.Drawing.Size(668, 382);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
@@ -155,7 +204,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox lObjectToActive;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer tInit;
     }
 }
--- CodeInject/MainMenu.cs
+++ CodeInject/MainMenu.cs
@@ -87,7 +87,6 @@ namespace CodeInject
 
 
             objectCount = (int*)(new IntPtr(GetInt32((GetBaseAdress() + 0x180D1B4))+0xEB4).ToPointer());
-            MessageBox.Show((*objectCount).ToString());
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -136,7 +135,7 @@ namespace CodeInject
 
         private void button1_Click_2(object sender, EventArgs e)
         {
-            resetList();
+            /*resetList();
             ActiveObject activeObject = (ActiveObject)Marshal.GetDelegateForFunctionPointer(new IntPtr((long)(GetBaseAdress() + 0x425a70)), typeof(ActiveObject));
             int* thisADDR = (int*)(GetBaseAdress() + 0x180D1B4);
             if (lNearObjects.SelectedIndex != -1 && *thisADDR != 0x0)
@@ -150,7 +149,9 @@ namespace CodeInject
                     *player.PosZ = *((Actor)lNearObjects.SelectedItem).PosZ;
                     activeObject(new IntPtr(*thisADDR).ToPointer(), new IntPtr(MonsterAdr(selectObjectIndex)).ToPointer());
                 }
-            }
+            }*/
+
+            timer2.Enabled = !timer2.Enabled;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -159,7 +160,7 @@ namespace CodeInject
             ActorList.Clear();
 
             int* firstObjectsElement = objectCount;
-            lObjectCount.Text = $"Count: {*objectCount}";
+            lObjectCount.Text = $"Count valid elements:  {ActorList.Where(x => lObjectToActive.Items.Contains(x.Name)).Count()}";
             firstObjectsElement++;
 
             for (int i = 0; i < *objectCount; i++)
@@ -177,11 +178,214 @@ namespace CodeInject
                 lNearObjects.Items.Add(actor);
                 firstObjectsElement++;
             }
+
+
+            if(ActorList.Where(x=> lObjectToActive.Items.Contains(x.Name)).Count()==0)
+            {
+
+                if (listBox1.SelectedIndex < listBox1.Items.Count-1)
+                {
+                    listBox1.SelectedIndex++;
+                }
+                else
+                {
+                    listBox1.SelectedIndex = 0;
+                }
+
+
+                Actor player = ActorList[0];
+                *player.PosX = ((Vector3)listBox1.SelectedItem).X;
+                *player.PosY = ((Vector3)listBox1.SelectedItem).Y;
+                *player.PosZ = ((Vector3)listBox1.SelectedItem).Z;
+
+
+                lNearObjects.Items.Clear();
+                ActorList.Clear();
+
+                firstObjectsElement = objectCount;
+   
+                firstObjectsElement++;
+
+                for (int i = 0; i < *objectCount; i++)
+                {
+                    Actor actor = new Actor
+                    {
+                        Name = Marshal.PtrToStringAnsi(new IntPtr(*firstObjectsElement + 0x30)),
+                        Addres = (*firstObjectsElement),
+                        Id = (*(firstObjectsElement + 0x74)),
+                        PosX = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x80).ToPointer(),
+                        PosZ = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x84).ToPointer(),
+                        PosY = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x88).ToPointer()
+                    };
+                    ActorList.Add(actor);
+                    lNearObjects.Items.Add(actor);
+                    firstObjectsElement++;
+                }
+
+
+            }
+            else
+            {
+                ActiveObject activeObject = (ActiveObject)Marshal.GetDelegateForFunctionPointer(new IntPtr((long)(GetBaseAdress() + 0x425a70)), typeof(ActiveObject));
+                int* thisADDR = (int*)(GetBaseAdress() + 0x180D1B4);
+                if (*thisADDR != 0x0)
+                {
+                    lNearObjects.SelectedItem = lNearObjects.Items.OfType<Actor>().FirstOrDefault(x => lObjectToActive.Items.Contains(x.Name));
+                    int selectObjectIndex = ActorList.FindIndex(x => ((Actor)lNearObjects.SelectedItem).Addres == x.Addres);
+                    if (selectObjectIndex != -1)
+                    {
+                        Actor player = ActorList[0];
+                        *player.PosX = *((Actor)lNearObjects.SelectedItem).PosX;
+                        *player.PosY = *((Actor)lNearObjects.SelectedItem).PosY;
+                        *player.PosZ = *((Actor)lNearObjects.SelectedItem).PosZ;
+                        activeObject(new IntPtr(*thisADDR).ToPointer(), new IntPtr(MonsterAdr(selectObjectIndex)).ToPointer());
+                    }
+                }
+            }
         }
 
         private void lNearObjects_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lObjectToActive.Items.Add(((Actor)lNearObjects.SelectedItem).Name);
+        }
+
+        List<Vector3> positionList = new List<Vector3>();
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            positionList.Add(new Vector3(*ActorList[0].PosX, *ActorList[0].PosY, *ActorList[0].PosZ));
+            listBox1.Items.Add(new Vector3(*ActorList[0].PosX, *ActorList[0].PosY, *ActorList[0].PosZ));
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           MessageBox.Show(ActorList.Where(x => lObjectToActive.Items.Contains(x.Name)).Count().ToString());
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            lNearObjects.Items.Clear();
+            ActorList.Clear();
+
+            int* firstObjectsElement = objectCount;
+            lObjectCount.Text = $"Count valid elements:  {ActorList.Where(x => lObjectToActive.Items.Contains(x.Name)).Count()}";
+            firstObjectsElement++;
+
+            for (int i = 0; i < *objectCount; i++)
+            {
+                Actor actor = new Actor
+                {
+                    Name = Marshal.PtrToStringAnsi(new IntPtr(*firstObjectsElement + 0x30)),
+                    Addres = (*firstObjectsElement),
+                    Id = (*(firstObjectsElement + 0x74)),
+                    PosX = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x80).ToPointer(),
+                    PosZ = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x84).ToPointer(),
+                    PosY = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x88).ToPointer()
+                };
+                ActorList.Add(actor);
+                lNearObjects.Items.Add(actor);
+                firstObjectsElement++;
+            }
+
+
+            if (ActorList.Where(x => lObjectToActive.Items.Contains(x.Name)).Count() == 0)
+            {
+
+                if (listBox1.SelectedIndex < listBox1.Items.Count - 1)
+                {
+                    listBox1.SelectedIndex++;
+                }
+                else
+                {
+                    listBox1.SelectedIndex = 0;
+                }
+
+
+                Actor player = ActorList[0];
+                *player.PosX = ((Vector3)listBox1.SelectedItem).X;
+                *player.PosY = ((Vector3)listBox1.SelectedItem).Y;
+                *player.PosZ = ((Vector3)listBox1.SelectedItem).Z;
+
+
+                lNearObjects.Items.Clear();
+                ActorList.Clear();
+
+                firstObjectsElement = objectCount;
+
+                firstObjectsElement++;
+
+                for (int i = 0; i < *objectCount; i++)
+                {
+                    Actor actor = new Actor
+                    {
+                        Name = Marshal.PtrToStringAnsi(new IntPtr(*firstObjectsElement + 0x30)),
+                        Addres = (*firstObjectsElement),
+                        Id = (*(firstObjectsElement + 0x74)),
+                        PosX = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x80).ToPointer(),
+                        PosZ = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x84).ToPointer(),
+                        PosY = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x88).ToPointer()
+                    };
+                    ActorList.Add(actor);
+                    lNearObjects.Items.Add(actor);
+                    firstObjectsElement++;
+                }
+
+
+            }
+            else
+            {
+                ActiveObject activeObject = (ActiveObject)Marshal.GetDelegateForFunctionPointer(new IntPtr((long)(GetBaseAdress() + 0x425a70)), typeof(ActiveObject));
+                int* thisADDR = (int*)(GetBaseAdress() + 0x180D1B4);
+                if (*thisADDR != 0x0)
+                {
+                    lNearObjects.SelectedItem = lNearObjects.Items.OfType<Actor>().FirstOrDefault(x => lObjectToActive.Items.Contains(x.Name));
+                    int selectObjectIndex = ActorList.FindIndex(x => ((Actor)lNearObjects.SelectedItem).Addres == x.Addres);
+                    if (selectObjectIndex != -1)
+                    {
+                        Actor player = ActorList[0];
+                        *player.PosX = *((Actor)lNearObjects.SelectedItem).PosX;
+                        *player.PosY = *((Actor)lNearObjects.SelectedItem).PosY;
+                        *player.PosZ = *((Actor)lNearObjects.SelectedItem).PosZ;
+                        activeObject(new IntPtr(*thisADDR).ToPointer(), new IntPtr(MonsterAdr(selectObjectIndex)).ToPointer());
+                    }
+                }
+            }
+        }
+
+        private void tInit_Tick(object sender, EventArgs e)
+        {
+            if(*objectCount>0)
+            {
+                lNearObjects.Items.Clear();
+                ActorList.Clear();
+
+                int* firstObjectsElement = objectCount;
+                lObjectCount.Text = $"Count valid elements:  {ActorList.Where(x => lObjectToActive.Items.Contains(x.Name)).Count()}";
+                firstObjectsElement++;
+
+                for (int i = 0; i < *objectCount; i++)
+                {
+                    Actor actor = new Actor
+                    {
+                        Name = Marshal.PtrToStringAnsi(new IntPtr(*firstObjectsElement + 0x30)),
+                        Addres = (*firstObjectsElement),
+                        Id = (*(firstObjectsElement + 0x74)),
+                        PosX = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x80).ToPointer(),
+                        PosZ = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x84).ToPointer(),
+                        PosY = (float*)new IntPtr(*firstObjectsElement + 0x30 + 0x88).ToPointer()
+                    };
+                    ActorList.Add(actor);
+                    lNearObjects.Items.Add(actor);
+                    firstObjectsElement++;
+                }
+                tInit.Enabled = false;
+            }
         }
     }
 }
